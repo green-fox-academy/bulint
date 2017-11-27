93ee395263cfb4702e522635e7bc8e2847337c41
bulint <bulint0324@gmail.com>
2017-11-27 18:17:07 +0100

    feat(exercise-15):add

---
+++ week-02/day-01/exercise-15/exercise-15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("If you give me a distance in kilometers, I will convert it to miles for you :) Otherwise I won't do anthing");
+            int kilometers = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(kilometers * 0.621371192);
+            Console.ReadLine();
+
+        }
+    }
+}
