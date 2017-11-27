d23c31b187db99f6c342404db843636db3f178ee
bulint <bulint0324@gmail.com>
2017-11-27 17:37:04 +0100

    feat(exercise-14):add

---
+++ week-02/day-01/exercise-14/exercise-14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.ReadLine();
+        }
+    }
+}
