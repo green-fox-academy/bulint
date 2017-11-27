9177b03e27fe1088616918d0aaee3290cf0677f9
bulint <bulint0324@gmail.com>
2017-11-27 11:48:46 +0100

    feat(week02day01):exercise one and two add

---
+++ week-02/day-01/exercise-02/exercise-02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Humpty Dumpty sat on a wall, ");
+            Console.WriteLine("Humpty Dumpty had a great fall.");
+            Console.WriteLine("Humpty Dumpty sat on a wall,");
+            Console.WriteLine("Couldn't put Humpty together again.");
+        }
+    }
+}
