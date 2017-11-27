e3cf40d32f2f30ec84584249b3c15577ce95c8af
bulint <bulint0324@gmail.com>
2017-11-27 11:51:44 +0100

    feat(day-01):exercise 3 add

---
+++ week-02/day-01/exercise-03/exercise-03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Szabolcs");
+            Console.WriteLine("Hello, Szilárd");
+            Console.WriteLine("Hello, Nóri");
+        }
+    }
+}
