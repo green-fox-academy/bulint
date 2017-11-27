642731ded9969e03f1a0fc8a97a7e8fd996bb2ad
bulint <bulint0324@gmail.com>
2017-11-27 15:15:34 +0100

    feat(day-01):exercise 5 add

---
+++ week-02/day-01/exercise-05/exercise-05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the result of 13 added to 22
+            Console.WriteLine(13 + 22);
+
+            // Print the result of 13 substracted from 22
+            Console.WriteLine(22 - 13);
+
+            // Print the result of 22 mutiplied by 13
+            Console.WriteLine(22 * 13);
+
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+            Console.WriteLine(22.0 / 13.0);
+
+            // Print the integer part of 22 divided by 13
+            Console.WriteLine(22 / 13);
+
+            // Print the reminder of 22 divided by 13
+            Console.WriteLine(22 % 13);
+
+            Console.ReadLine();
+        }
+    }
+}
