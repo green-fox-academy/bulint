4cd55e3420193ec1d881ab64d70d83d30dc85e35
bulint <bulint0324@gmail.com>
2017-11-27 16:29:26 +0100

    feat(exercise-08):add

---
+++ week-02/day-01/exercise-08/exercise-08/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the variables
+            int a = 123;
+            int b = 526;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
