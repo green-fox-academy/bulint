7424ca34d1ef40a7386d8975d63fa9d0880edb16
bulint <bulint0324@gmail.com>
2017-11-27 16:08:25 +0100

    feat(exercise-07):add

---
+++ week-02/day-01/exercise-07/exercise-07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            // And print it like this: "My favorite number is: 8"
+
+            int favouriteNumber = 14;
+            Console.WriteLine("My favourite number is: " + favouriteNumber);
+            Console.ReadLine();
+        }
+    }
+}
