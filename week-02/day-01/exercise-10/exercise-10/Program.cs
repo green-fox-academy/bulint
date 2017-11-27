8381721c7bba8a86d1655597b14cf3fff2ec77e4
bulint <bulint0324@gmail.com>
2017-11-27 16:41:03 +0100

    feat(exercise-10):add

---
+++ week-02/day-01/exercise-10/exercise-10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Whether you are married or not as a boolean
+
+            string myName = "Bálint";
+            int myAge = 23;
+            double myHeight = 1.8;
+            bool married = false;
+
+            Console.WriteLine(myName);
+            Console.WriteLine(myAge);
+            Console.WriteLine(myHeight);
+            Console.WriteLine(married);
+            Console.ReadLine();
+        }
+    }
+}
