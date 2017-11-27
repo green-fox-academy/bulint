0124963d14dedebe6ea2200583c659fb08e216b2
bulint <bulint0324@gmail.com>
2017-11-27 14:30:29 +0100

    feat(day-01):exercise 4 add

---
+++ week-02/day-01/exercise-04/exercise-04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bálint Bodnár");
+            Console.WriteLine("23");
+            Console.WriteLine("1,8");
+            Console.ReadKey();
+        }
+    }
+}
