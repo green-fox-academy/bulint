5ada3235f642af2fa9cca4d55e12db638c235979
bulint <bulint0324@gmail.com>
2017-11-28 12:14:48 +0100

    feat(appenda):add

---
+++ week-02/day-02/AppendA/AppendA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void AppendA(string am)
+        {
+            Console.WriteLine(am + "a");
+        }
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+
+            string am = "kuty";
+            AppendA(am);
+            Console.ReadLine();
+        }
+    }
+}
