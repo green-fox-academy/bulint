9dac103656e5d260583a41014f97e11e279158c8
bulint <bulint0324@gmail.com>
2017-11-28 15:25:28 +0100

    feat(factorial):add

---
+++ week-02/day-02/Factorial/Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+
+            int n = 5;
+            Factorio(n);
+            Console.ReadLine();
+        }
+
+        static void Factorio(int factorio)
+        {
+
+            int sum = 1;
+
+            for (int i = 1; i < factorio + 1; i++)
+            {
+                sum *= i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
