c51775eee161fd6d79f6bca46cabb060d311b320
bulint <bulint0324@gmail.com>
2017-11-28 14:38:42 +0100

    feat(summing):add

---
+++ week-02/day-02/Summing/Summing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+
+            int n = 5;
+            Sum(n);
+            Console.ReadLine();
+        }
+
+        static void Sum(int summary)
+        {
+            int sum = 0;
+
+            for (int i = 0; i <= summary; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+    }
+}
