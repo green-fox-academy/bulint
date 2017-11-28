188cec908f02c3385df1024a1996a659fd596d84
bulint <bulint0324@gmail.com>
2017-11-28 11:46:30 +0100

    feat(doubling):add

---
+++ week-02/day-02/Doubling/Doubling/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubling
+{
+    class Program
+    {
+        public static int Doubling(int i)
+        {
+            return i * 2;
+        }
+
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            int ak = 123;
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+
+        }
+    }
+}
