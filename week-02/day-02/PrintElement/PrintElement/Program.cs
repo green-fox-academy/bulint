eb09afabf7dee397fd60ee5af8082cbaea5c9666
bulint <bulint0324@gmail.com>
2017-11-28 17:32:09 +0100

    feat(printelement):add

---
+++ week-02/day-02/PrintElement/PrintElement/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintElement
+{
+    class Program
+    {
+        // - Create an array variable named `af`
+        //   with the following content: `[4, 5, 6, 7]`
+        // - Print all the elements of `af`
+
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            foreach (int numbers in af)
+            {
+                Console.WriteLine(numbers);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
