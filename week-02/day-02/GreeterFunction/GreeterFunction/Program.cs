e271f0690bd68d0ec08c01b5a4e28b28feadd8e2
bulint <bulint0324@gmail.com>
2017-11-28 12:07:58 +0100

    feat(greeterfunction):add

---
+++ week-02/day-02/GreeterFunction/GreeterFunction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterFunction
+{
+    class Program
+    {
+        static void Greet(string al)
+        {
+            Console.WriteLine("Greetings dear, " + al + "!");
+            Console.ReadLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+
+            string al = "Greenfox";
+            Greet(al);
+        }
+    }
+}
