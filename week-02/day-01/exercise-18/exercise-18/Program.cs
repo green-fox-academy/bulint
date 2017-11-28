77578663de53d1b0ba6f2a6c977038c48f49e926
bulint <bulint0324@gmail.com>
2017-11-28 09:52:51 +0100

    feat(exercise-18):add

---
+++ week-02/day-01/exercise-18/exercise-18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+            Console.WriteLine("Give me a number");
+            int number = Int32.Parse(Console.ReadLine());
+            if (number % 2 != 0 && number % number == 0 && number % 3 != 0 && number % 3 != 0 && number % 3 != 0)
+            {
+                Console.WriteLine("The number is odd");
+            }
+            else
+            {
+                Console.WriteLine("The number is even");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
