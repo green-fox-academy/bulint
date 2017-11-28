cadff83dc8136e0773c206bd1291c6fe7b2f2729
bulint <bulint0324@gmail.com>
2017-11-28 17:24:12 +0100

    feat(incrementelement):add

---
+++ week-02/day-02/incrementelement/incrementelement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incrementelement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+
+            int[] t = { 1, 2, 3, 4, 5 };
+            t[2]++;
+            Console.WriteLine(t[2]);
+            Console.ReadLine();
+        }
+    }
+}
