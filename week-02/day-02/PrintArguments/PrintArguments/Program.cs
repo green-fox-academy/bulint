0355ce5e946d07d1bde520fd923c53317cb31199
bulint <bulint0324@gmail.com>
2017-11-28 16:49:04 +0100

    feat(printarguments):add

---
+++ week-02/day-02/PrintArguments/PrintArguments/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// - Create a function called `printer`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printer("first")
+// printer("first", "second")
+// printer("first", "second", "third", "fourh")
+// ...
+
+namespace PrintArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] array = { "i", "dont", "understand", "shit" };
+            Printer(array);
+            Console.ReadLine();
+        }
+
+        static void Printer(string[] myArray)
+        {
+
+            foreach (string item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
