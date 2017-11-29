27fba449bf0d0ed88563f4a4a03566daefb8a9ee
bulint <bulint0324@gmail.com>
2017-11-29 12:21:53 +0100

    feat(strings02):add

---
+++ week-02/day-03/Strings02/Strings02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings02
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+            url = url.Replace("bots", "odds");
+            url = url.Insert(5, ":");
+
+            // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+            // Also, the URL is missing a crutial component, find out what it is and insert it too!
+            // Try to solve it more than once using different string functions!
+
+            Console.WriteLine(url);
+            Console.ReadLine();
+        }
+    }
+}
