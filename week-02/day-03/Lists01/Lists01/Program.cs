46b40d2c6755485fa7ccc998f5ded661c6b336f8
bulint <bulint0324@gmail.com>
2017-11-29 16:00:31 +0100

    feat(lists01):add

---
+++ week-02/day-03/Lists01/Lists01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+            planetList.Insert(5, "Saturn");
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            foreach (var item in planetList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- week-02/day-03/Strings04/Strings04/Program.cs
+++ week-02/day-03/Strings04/Strings04/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Text;
 
-namespace String04
+namespace Strings04
 {
     class Program
     {
