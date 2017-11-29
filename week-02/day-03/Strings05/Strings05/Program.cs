ccedc27f13509a76e176b944e76861efc0ac04df
bulint <bulint0324@gmail.com>
2017-11-29 15:37:54 +0100

    feat(strings05):add

---
+++ week-02/day-03/Strings05/Strings05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Strings05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Reverse(reversed);
+            Console.ReadLine();
+        }
+        public static void Reverse(string reverser)
+        {
+            string result = String.Empty;
+            char[] array = reverser.ToCharArray();
+            for (int i = 80; i > -1 ; i--)
+            {
+                result += array[i];
+            }
+            reverser.Remove(0, 80);
+            Console.WriteLine(result);
+        }
+    }
+}
