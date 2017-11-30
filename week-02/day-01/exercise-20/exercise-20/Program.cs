9e8bcb38257d4075edd7961a9495e6ce9e08fe87
bulint <bulint0324@gmail.com>
2017-11-30 13:53:13 +0100

    feat(exercise-20):add

---
+++ week-02/day-01/exercise-20/exercise-20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("The bigger number is: " + number1);
+            }
+            else
+            {
+                Console.WriteLine("The bigger number is: " + number2);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
