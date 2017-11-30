e66b78c5a653967fbe5022300980b34384b773a3
bulint <bulint0324@gmail.com>
2017-11-30 10:10:11 +0100

    feat(exercise-23):add

---
+++ week-02/day-01/exercise-23/exercise-23/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercise_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i + ". I won't cheat on the exam");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
