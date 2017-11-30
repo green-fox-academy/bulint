2eb72267abe41ee83d3e3214cda7c2f037f36d38
bulint <bulint0324@gmail.com>
2017-11-30 11:54:14 +0100

    feat(exercise-28):add

---
+++ week-02/day-01/exercise-28/exercise-28/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercise_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+
+            Random random = new Random();
+            int number = random.Next(1, 10);
+            Console.WriteLine("The random number is: " + number);
+            string triangle = String.Empty;
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(triangle += "*");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
