bba5232dd04dc29478247cbcca16a0f20203e6dd
bulint <bulint0324@gmail.com>
2017-11-30 13:49:22 +0100

    feat(exercise-17):add

---
+++ week-02/day-01/exercise-17/exercise-17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            int Sum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("I want a number: ");
+                Sum += int.Parse(Console.ReadLine());
+            }
+
+            int Average = Sum / 5;
+
+            Console.WriteLine("Sum: " + Sum + ", Average: " + Average);
+            Console.ReadLine();
+        }
+    }
+}
