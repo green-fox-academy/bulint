3a9b4a01d3c1061ec0d511193a608e7338ad0f78
bulint <bulint0324@gmail.com>
2017-11-30 12:26:28 +0100

    feat(exercise-09):add

---
+++ week-02/day-01/exercise-09/exercise-09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+            heightInM = heightInM * heightInM;
+            double BMI = massInKg / heightInM;
+            Console.WriteLine(BMI);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-01/exercise-32/exercise-32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercise_32
+{
+    class Program
+    {
+        public static void Main(string[] Args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+            Console.WriteLine("Give me a number matey: ");
+            int number = int.Parse(Console.ReadLine());
+            string square = String.Empty;
+            for (int i = 0; i < number; i++)
+            {
+
+            }
+
+        }
+    }
+}
