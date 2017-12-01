4dbad66e7f6f9b7d3fe259e1770a4a2c2b20721d
bulint <bulint0324@gmail.com>
2017-12-01 11:31:35 +0100

    feat(SwapElements): add

---
+++ week-02/day-02/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+
+            string[] abc = { "first", "second", "third" };
+            abc.SetValue("third", 0);
+            abc.SetValue("first", 2);
+
+            foreach (var item in abc)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
