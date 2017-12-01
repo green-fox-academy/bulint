99ee498904033957f5dda4d7d94009a801659024
bulint <bulint0324@gmail.com>
2017-12-01 14:33:20 +0100

    feat(lists06): add

---
+++ week-02/day-03/Lists06/Lists06/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists06
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list1 = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+
+            // Check if list contains all of the following elements: 4,8,12,16
+            // Print "true" if it contains all, otherwise print "false"
+            // Can you do both the different approaches you tried in the previous one?
+
+            var list2 = new List<int> { 4, 8, 12, 16 };
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                for (int j = 0; j < list2.Count; j++)
+                {
+                    if (list1[i] == list2[j])
+                    {
+                        list2[j] = 0;
+                    }
+                }
+            }
+
+            if (list2[0] == 0 && list2[1] == 0 && list2[2] == 0 && list2[3] == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
