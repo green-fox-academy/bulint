48f74099a8dc4212c14cf0a5fecda479ae35781b
bulint <bulint0324@gmail.com>
2017-12-01 12:09:40 +0100

    feat(lists03): add

---
+++ week-02/day-03/Lists03/Lists03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists03
+{
+    public class Program
+    {
+        public class AppendLetter
+        {
+            public static void Main(string[] args)
+            {
+                var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+
+                // Add "a" to every string in the far list.
+
+                for (int i = 0; i < far.Count; i++)
+                {
+                    far[i] += "a";
+                }
+
+                foreach (var item in far)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
