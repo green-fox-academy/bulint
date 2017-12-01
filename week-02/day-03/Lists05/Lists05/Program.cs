ceb007d5b61c323b3f298749e886c416b0b1ae3c
bulint <bulint0324@gmail.com>
2017-12-01 12:25:08 +0100

    feat(Lists05): add

---
+++ week-02/day-03/Lists05/Lists05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hoorray!!!11one");
+            }
+            else
+            {
+                Console.WriteLine("Noooooooooooo!!!11one");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
