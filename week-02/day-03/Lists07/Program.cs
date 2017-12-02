f3410ff810b9476485b92a5694bef9567d9c3365
bulint <bulint0324@gmail.com>
2017-12-02 17:21:57 +0100

    feat(lists07): add

---
+++ week-02/day-03/Lists07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+            list[2] = "cannot";
+            list[5] = "do";
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
