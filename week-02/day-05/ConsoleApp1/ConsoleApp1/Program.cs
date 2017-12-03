990f06b006755591399b63339ec86e195a6aa373
bulint <bulint0324@gmail.com>
2017-12-03 14:21:13 +0100

    feat(PalindromeBuilder)

---
+++ week-02/day-05/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a series of characters!!11one:  ");
+            string input = Console.ReadLine();
+            CreatePalindrome(input);
+            Console.ReadLine();
+        }
+
+        static void CreatePalindrome(string palindrome)
+        {
+            char[] charray = palindrome.ToCharArray();
+            for (int i = palindrome.Length - 1; i >= 0; i--)
+            {
+                palindrome += charray[i];
+            }
+            Console.WriteLine(palindrome);
+        }
+    }
+}
