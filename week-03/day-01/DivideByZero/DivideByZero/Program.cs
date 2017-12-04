b0ecff268edf1ec467b121a079246870e7ad628c
bulint <bulint0324@gmail.com>
2017-12-04 12:17:26 +0100

    feat(dividebyzero): add

---
+++ week-03/day-01/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number: ");
+            int input = int.Parse(Console.ReadLine());
+            if (input == 0)
+            {
+                Divider(input);
+            }
+
+        }
+        static void Divider(int number)
+        {
+            try
+            { 
+                int ten = 10;
+                int result = ten / number;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Fail");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
