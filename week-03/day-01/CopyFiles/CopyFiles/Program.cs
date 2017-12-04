dcdbc82efb117416bcec59c5a6bea42a18fa43b3
bulint <bulint0324@gmail.com>
2017-12-04 17:19:35 +0100

    feat(writemultiplelines): add

---
+++ week-03/day-01/CopyFiles/CopyFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+// Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
