a50085a6457aa81b45338d5725aedad8e2bd317e
bulint <bulint0324@gmail.com>
2017-12-04 14:41:13 +0100

    feat(countlines): add

---
+++ week-03/day-01/CountLines/CountLines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\L\Desktop\doksi.txt";
+            try
+            {
+                Console.WriteLine(NumberOfLines(path));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(0);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static int NumberOfLines(string fileName)
+        {
+            string[] content = File.ReadAllLines(fileName);
+            int numberOfLines = content.Length;
+            return numberOfLines;
+        } 
+    }
+}
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
