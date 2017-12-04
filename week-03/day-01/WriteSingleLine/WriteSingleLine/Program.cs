6a1ff8c291623e050a84ac7821fff0318c7350d1
bulint <bulint0324@gmail.com>
2017-12-04 15:44:24 +0100

    feat(writesingleline): add

---
+++ week-03/day-01/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\L\Documents";
+            try
+            {
+                using (StreamWriter outputFile = new StreamWriter(path + @"\doksi.txt", true))
+                {
+                    outputFile.WriteLine("Bodnár Bálint");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write the file: " + path);
+            }
+            finally
+            {
+                string[] content = File.ReadAllLines(path + @"\doksi.txt");
+                foreach (var item in content)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+// Open a file called "my-file.txt"
+// Write your name in it as a single line
+// If the program is unable to write the file,
+// then it should print an error message like: "Unable to write file: my-file.txt"
