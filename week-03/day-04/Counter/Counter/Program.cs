bc35bfe0d5332405cd55a685117204ced93f9d87
bulint <bulint0324@gmail.com>
2017-12-07 11:28:15 +0100

    feat(Counter): add

---
+++ week-03/day-04/Counter/Counter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 666;
+            RecursiveMethod(number);
+            Console.ReadLine();
+        }
+        static int RecursiveMethod(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine(number);
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(number);
+                return RecursiveMethod(--number);
+            }
+        }
+    }
+}
---
+++ week-03/day-04/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            NumberAdder(number);
+        }
+        static int NumberAdder(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                int addedNumbers =+ number;
+            }
+            Console.WriteLine(number);
+            return NumberAdder(number);
+        }
+    }
+}
