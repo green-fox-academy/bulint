d66dd644385cc8edece4827d288800bb662b076f
bulint <bulint0324@gmail.com>
2017-12-07 17:55:55 +0100

    feat(SumDigit): add

---
+++ week-03/day-04/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 126;
+            int sum = 0;
+            Console.WriteLine(SumDigit(number, sum));
+            Console.ReadLine();
+
+        }
+        static int SumDigit(int number, int sum)
+        {
+            if (number % 10 < 1)
+            {
+                return sum;
+            }
+            return SumDigit(number / 10, sum += (number % 10));
+
+        }
+    }
+}
