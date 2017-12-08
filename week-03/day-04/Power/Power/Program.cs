2ccbf1ea5eb62cf0d4705c04591807e47f16884e
bulint <bulint0324@gmail.com>
2017-12-08 11:53:47 +0100

    feat(Power): add

---
+++ week-03/day-04/Power/Power/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 6;
+            int power = 2;
+            ToTheSky(power, number);
+            Console.ReadLine();
+        }
+        public static void ToTheSky(int power, int number)
+        {
+            if (power == 1)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                ToTheSky(--power, number * number);
+            }
+        }
+    }
+}
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
