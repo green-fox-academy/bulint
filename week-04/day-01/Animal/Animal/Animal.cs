0a0e5245448bbe98f048f49349ee5644b1dc31bf
bulint <bulint0324@gmail.com>
2017-12-11 13:21:19 +0100

    feat(Animal): add

---
+++ week-04/day-01/Animal/Animal/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int hunger = 50;
+        public int thirst = 50;
+
+        public void Eat()
+        {
+            hunger--;
+        }
+
+        public void Drink()
+        {
+            thirst--;
+        }
+
+        public void Play()
+        {
+            thirst++;
+            hunger++;
+        }
+    }
+}
---
+++ week-04/day-01/Animal/Animal/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wildAnimal = new Animal();
+            wildAnimal.Drink();
+            wildAnimal.Eat();
+            wildAnimal.Play();
+            wildAnimal.Play();
+            wildAnimal.Play();
+            wildAnimal.Eat();
+            wildAnimal.Drink();
+            Console.WriteLine(wildAnimal.hunger);
+            Console.WriteLine(wildAnimal.thirst);
+            Console.ReadLine();
+        }
+    }
+}
