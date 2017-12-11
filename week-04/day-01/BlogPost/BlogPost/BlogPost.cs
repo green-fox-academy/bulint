333832e0def26645674bb881215045c5438da069
bulint <bulint0324@gmail.com>
2017-12-11 12:17:12 +0100

    feat(BlogPost): update

--- week-04/day-01/BlogPost/BlogPost/BlogPost.cs
+++ week-04/day-01/BlogPost/BlogPost/BlogPost.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlogPost
+﻿namespace BlogPost
 {
     class BlogPost
     {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
     }
 }
