9774a6af84bfcf6d9749c0b41cb4936e763a83a9
bulint <bulint0324@gmail.com>
2017-12-11 16:10:58 +0100

    feat(Dice): add

---
+++ week-04/day-01/Dice/Dice/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Dice
+{
+    public class Dice
+    {
+        public static Random random = new Random();
+        public int diceValue { get; set; }
+
+        public void Roll()
+        {
+            diceValue = random.Next(1, 7);
+        }
+
+        public void ReRoll()
+        {
+            diceValue = random.Next(1, 7);
+        }
+
+        public int GetCurrent(Dice dice)
+        {
+            return dice.diceValue;
+        }
+    }
+}
---
+++ week-04/day-01/Dice/Dice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var diceList = new List<Dice>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                diceList.Add(new Dice());
+            }
+            for (int j = 0; j < 6; j++)
+            {
+                diceList[j].Roll();
+            }
+            for (int l = 0; l < 6; l++)
+            {
+                Console.WriteLine(diceList[l].GetCurrent(diceList[l]));
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
