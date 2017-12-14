5aaf5b69c7d50c246e95af6af32f5aeedabc196d
bulint <bulint0324@gmail.com>
2017-12-14 09:13:27 +0100

    feat(DrawDiamond): add

---
+++ week-02/day-01/DrawDiamond/DrawDiamond/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DrawDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                for (int j = i; j < input; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int l = 0; l < i * 2 - 1; l++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = input - 1; i >= 1; i--)
+            {
+                for (int j = input - 1; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int l = 0; l < i * 2 - 1; l++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
