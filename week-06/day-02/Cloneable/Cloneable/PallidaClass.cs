14dd9d4c426de00b4bb4b2c869a0af314065ac02
bulint <bulint0324@gmail.com>
2018-01-02 15:46:18 +0100

    feat(Cloneable): finish

---
+++ week-06/day-02/Cloneable/Cloneable/PallidaClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenFoxOrganization
+{
+    class PallidaClass
+    {
+        public string className;
+        public List<Person> students = new List<Person>();
+        public List<Person> mentors = new List<Person>();
+
+        public PallidaClass(string className)
+        {
+            this.className = className;
+            students.Clear();
+            mentors.Clear();
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void AddMentor(Mentor mentor)
+        {
+            mentors.Add(mentor);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Pallida " + className + " class has " + students.Count + " students and " + mentors.Count + " mentors.");
+        }
+    }
+}
