da993823d8119fbe1faa8b46729661e256fc6a37
bulint <bulint0324@gmail.com>
2018-01-02 15:35:17 +0100

    feat(Comparable): finish

---
+++ week-06/day-02/Comparable/Comparable/Domino.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Comparable
+{
+    public class Domino : IComparable
+    {
+        private readonly int[] Values;
+        private int A { get; set; }
+
+        public Domino(int valueA, int valueB)
+        {
+            Values = new int[] { valueA, valueB };
+            A = valueA;
+        }
+
+        public int CompareTo(object obj)
+        {
+            var otherDomino = obj as Domino;
+
+            if (A > otherDomino.A)
+            {
+                return 1;
+            }
+            else if (A < otherDomino.A)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
---
+++ week-06/day-02/Comparable/Comparable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dominoes = new List<Domino>
+
+            {
+                new Domino(5, 2),
+                new Domino(4, 6),
+                new Domino(1, 5),
+                new Domino(6, 7),
+                new Domino(2, 4),
+                new Domino(7, 1)
+            };
+
+            dominoes.Sort();
+
+            foreach (var domino in dominoes)
+            {
+                foreach (var values in domino.GetValues())
+                {
+                    Console.WriteLine(values);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
