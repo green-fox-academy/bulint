1390aed7809a9e30f3b951f584736707d2123a93
bulint <bulint0324@gmail.com>
2018-01-03 16:55:36 +0100

    feat(Exercise5): add query syntax

--- week-06/day-03/Exercise5/Exercise5/Program.cs
+++ week-06/day-03/Exercise5/Exercise5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Exercise5
@@ -9,10 +10,21 @@ namespace Exercise5
         {
             int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
 
-            var frequencies = numbers.GroupBy(n => n)
-                .ToDictionary(n => n.Key, n => n.Count());
+            //var frequencies = numbers.GroupBy(n => n)
+            //    .ToDictionary(n => n.Key, n => n.Count());
 
-            foreach (var f in frequencies)
+            //foreach (var f in frequencies)
+            //{
+            //    Console.WriteLine(f);
+            //}
+
+            var frequenciesQuery =
+                from n in numbers
+                group n by n into g
+                select g;
+            Dictionary<int, int> frequenciesDictionary = frequenciesQuery.ToDictionary(n => n.Key, n => n.Count());
+
+            foreach (var f in frequenciesDictionary)
             {
                 Console.WriteLine(f);
             }
