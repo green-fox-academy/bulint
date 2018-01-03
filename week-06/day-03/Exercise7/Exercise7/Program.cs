ea3635adb44e0970b99bb1a18f4b0708b1e64df6
bulint <bulint0324@gmail.com>
2018-01-03 21:56:27 +0100

    feat(Exercise7): i dont know what im doing

---
+++ week-06/day-03/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var filteredCities = cities.Where(s => cities[0] == "A")
+                && cities.Where(s => cities[s.Length] == "I");
+
+        }
+    }
+}
