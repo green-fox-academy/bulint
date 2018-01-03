d0f364cfa6be0482b523161eb8c6a8b47df9804a
bulint <bulint0324@gmail.com>
2018-01-03 16:02:17 +0100

    feat(Exercise5): finish

---
+++ week-06/day-03/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var frequencies = numbers.GroupBy(n => n)
+                .ToDictionary(n => n.Key, n => n.Count());
+
+            foreach (var f in frequencies)
+            {
+                Console.WriteLine(f);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
