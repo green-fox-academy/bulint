8796e71ed45a441d7a28b19e8b1fd62029ad6025
bulint <bulint0324@gmail.com>
2018-01-03 14:27:23 +0100

    feat(Exercise3): finish

---
+++ week-06/day-03/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            numbers.Where(n => n >= 0)
+                .Select(n => n * n)
+                .ToList()
+                .ForEach(i => Console.WriteLine(i));
+
+            Console.ReadLine();
+        }
+    }
+}
