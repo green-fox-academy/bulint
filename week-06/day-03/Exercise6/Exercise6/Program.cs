ff8bf0852758acabeae368e1bf432bcd1a6f39fb
bulint <bulint0324@gmail.com>
2018-01-03 17:24:31 +0100

    feat(Exercise6): finish

---
+++ week-06/day-03/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToCountLetterFrequencesIn = "kummerspeck";
+            var frequencies = stringToCountLetterFrequencesIn.ToCharArray()
+                .GroupBy(f => f)
+                .ToDictionary(f => f.Key, f => f.Count());
+
+            foreach (var f in frequencies)
+            {
+                Console.WriteLine(f);
+            }
+
+            var frequenciesQuery =
+                from f in stringToCountLetterFrequencesIn
+                group f by f into g
+                select g;
+
+            foreach (var f in frequenciesQuery.ToDictionary(f => f.Key, f => f.Count()))
+            {
+                Console.WriteLine(f);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
