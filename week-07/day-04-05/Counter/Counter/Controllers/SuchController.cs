6b9e4c56fc875d8c4d26cf91804f9fc5a94d05e1
bulint <bulint0324@gmail.com>
2018-01-11 12:13:01 +0100

    feat(counter): finish

---
+++ week-07/day-04-05/Counter/Counter/Controllers/SuchController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Controllers
+{
+    [Route("")]
+    public class SuchController : Controller
+    {
+        private Counter counter;
+
+        public SuchController(Counter counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost("increase")]
+        public IActionResult IncreaseCounter()
+        {
+            counter.Number++;
+            return RedirectToAction("Index");
+        }
+    }
+}
