e90e2cb0efa3dbf6798d9245c110456ff6416c21
bulint <bulint0324@gmail.com>
2018-01-11 14:21:26 +0100

    feat(greetsomeone): finish

---
+++ week-07/day-04-05/GreetSomeone/GreetSomeone/Controllers/WebController.cs
@@ -0,0 +1,39 @@
+﻿using GreetSomeone.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeone.Controllers
+{
+    [Route("")]
+    public class WebController : Controller
+    {
+        private Greet greet;
+
+        public WebController(Greet greet)
+        {
+            this.greet = greet;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(greet);
+        }
+
+        [HttpPost("redirect")]
+        public IActionResult PostMethod(string name)
+        {
+            greet.Name = name;
+            return RedirectToAction("Greet");
+        }
+
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return View(greet);
+        }
+    }
+}
---
+++ week-07/day-04-05/GreetSomeone/GreetSomeone/Models/Greet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeone.Models
+{
+    public class Greet
+    {
+        public string Name { get; set; }
+
+        //public Greet(string name)
+        //{
+        //    Name = name;
+        //}
+
+    }
+}
